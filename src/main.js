dfe299d1c9611eda5d97d33dd6408f2fd72f6852
wangheng <wang.heng@asiainfo-sec.com>
2022-07-12 22:21:05 +0800

    fix: import

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Message from './plugins/message'
 
 Vue.config.productionTip = false
 
-import Message from './plugins/message'
 Vue.use(Message)
 
 new Vue({
